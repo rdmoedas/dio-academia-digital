289bb756574935aa34c7ea5b7733c881765744e2
Rodolfo Santana <rdmoedas@gmail.com>
2023-04-29 15:32:49 -0300

    fix: fix imports

--- src/main/java/me/dio/academia/digital/infra/jackson/ObjectMapperConfig.java
+++ src/main/java/me/dio/academia/digital/infra/jackson/ObjectMapperConfig.java
@@ -1,10 +1,10 @@
 package me.dio.academia.digital.infra.jackson;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import me.dio.academia.digital.infra.jackson.deser.LocalDateDeserializer;
+import me.dio.academia.digital.infra.jackson.deser.LocalDateTimeDeserializer;
+import me.dio.academia.digital.infra.jackson.ser.LocalDateSerializer;
+import me.dio.academia.digital.infra.jackson.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
