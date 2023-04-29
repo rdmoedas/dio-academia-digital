5d7b18834787a760711f0c7de1a1b214a9c70a09
Rodolfo Santana <rdmoedas@gmail.com>
2023-04-29 16:38:28 -0300

    feat: add dotenv-java configuration and .env.example

--- src/main/java/me/dio/academia/digital/AcademiaDigitalApplication.java
+++ src/main/java/me/dio/academia/digital/AcademiaDigitalApplication.java
@@ -2,11 +2,18 @@ package me.dio.academia.digital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 
 @SpringBootApplication
 public class AcademiaDigitalApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("server.port", dotenv.get("SERVER_PORT"));
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(AcademiaDigitalApplication.class, args);
 	}
 
