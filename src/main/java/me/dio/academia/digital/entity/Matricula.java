6f8705515dc8f8aacaaa07f0a5c32222359c0aa6
Rodolfo Santana <rdmoedas@gmail.com>
2023-04-29 15:32:32 -0300

    feat: configure entities

--- src/main/java/me/dio/academia/digital/entity/Matricula.java
+++ src/main/java/me/dio/academia/digital/entity/Matricula.java
@@ -1,11 +1,24 @@
 package me.dio.academia.digital.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_matriculas")
 public class Matricula {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne
+    @JoinColumn(name = "aluno_id")
     private Aluno aluno;
 
     private LocalDateTime dataDaMatricula = LocalDateTime.now();
