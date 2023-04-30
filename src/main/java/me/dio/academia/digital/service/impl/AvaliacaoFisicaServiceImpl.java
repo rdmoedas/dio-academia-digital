1bf2f03425143d3132e2e24bffd54d2ed5acf96c
Rodolfo Santana <rdmoedas@gmail.com>
2023-04-30 02:14:22 -0300

    feat: add avaliacaoFisica routes and methods

---
+++ src/main/java/me/dio/academia/digital/service/impl/AvaliacaoFisicaServiceImpl.java
@@ -0,0 +1,58 @@
+package me.dio.academia.digital.service.impl;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaUpdateForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.repository.AvaliacaoFisicaRepository;
+import me.dio.academia.digital.service.IAvaliacaoFisicaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AvaliacaoFisicaServiceImpl implements IAvaliacaoFisicaService {
+
+    @Autowired
+    private AvaliacaoFisicaRepository avaliacaoFisicaRepository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Override
+    public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
+        AvaliacaoFisica avaliacao = new AvaliacaoFisica();
+
+        // TODO: Implementar tratativa de erro caso id não exista.
+        Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+
+        avaliacao.setAluno(aluno);
+        avaliacao.setAltura(form.getAltura());
+        avaliacao.setPeso(form.getPeso());
+
+        // Uma boa prática é retornar uma view, para evitar mostrar dados como senha.
+        return avaliacaoFisicaRepository.save(avaliacao);
+    }
+
+    @Override
+    public AvaliacaoFisica get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<AvaliacaoFisica> getAll() {
+        return null;
+    }
+
+    @Override
+    public AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
