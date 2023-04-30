050966f1ad426ae16a7c969a66f5dc945daa25f7
Rodolfo Santana <rdmoedas@gmail.com>
2023-04-30 02:12:26 -0300

    feat: add AlunoForm validations

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -1,13 +1,34 @@
 package me.dio.academia.digital.entity.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlunoForm {
+
+    @NotBlank(message = "Preencha o campo corretamente.")
+    @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
     private String nome;
 
+    @NotBlank(message = "Preencha o campo corretamente.")
+//    @CPF(message = "'${validatedValue}' é inválido!")
     private String cpf;
 
+    @NotBlank(message = "Preencha o campo corretamente.")
+    @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
     private String bairro;
 
+    @NotNull(message = "Preencha o campo corretamente.")
+    @Past(message = "Data '${validateValue}' é inválida.")
     private LocalDate dataDeNascimento;
 }
